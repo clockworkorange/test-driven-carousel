7126b0cd5aa2d9c8d4c35da37d55ccbff08d6a5e
Julio Ripoll Moreno <julio.ripoll@osoco.es>
2019-07-22 15:52:01 +0200

    :wrench: Initial ESLint and Prettier config

---
+++ wallaby.js
@@ -0,0 +1,15 @@
+module.exports = function (wallaby) {
+  return {
+    testFramework: 'jest',
+    env: {
+      type: 'node',
+    },
+    tests: ['src/tests/**/*.test.js'],
+    files: ['src/**/*.js', '!**/*.test.js', '!**/.*'],
+  };
+};
+compilers: {
+  '**/*.js'
+:
+  wallaby.compilers.babel(),
+}
